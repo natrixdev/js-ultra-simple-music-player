ca5d306c5b0f09bd7e48ee4d0670dbd896345534
natrix_dev <discordlenko@gmail.com>
2022-02-01 18:10:15 +0100

    Create music_player.js

---
+++ music_player.js
@@ -0,0 +1,30 @@
+// I will use my module so let's import it 
+
+const music = require("music-player.1")
+
+// specify your music link (youtube or directory (on your machine)
+
+let yt_playlist_link = ("YT LINK FOR PLAYLIST") 
+let dir_music = ("C:/User/Desktop/Music_Exemple")
+
+// run the music with a little page !
+
+//page init 
+music.new(page, 52; 87)
+music.init(controls: prev, pause, play, next)
+
+function ("music-loop"){
+  music.new(button-loop & loop music)
+}
+
+//play music 
+//you can change loop on the page 
+music.play(loop: no")
+           music.keep-alive
+           music.exit-btn
+           
+           
+           
+           
+      //natrixdev - 2022
+           
